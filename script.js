9e1493885daf9e5186e4a4764883cdf7d0d48a2a
heldrethtw <141576187+heldrethtw@users.noreply.github.com>
2023-09-18 04:47:04 -0400

    Javascript 1.2 task

---
+++ script.js
@@ -0,0 +1,27 @@
+let pokemonList = []
+
+//Pokemon 1
+const pokemon1 =
+    {name: "Bulbasaur",
+     height: 7,
+     types: [grass, poison]
+};
+pokemonList.push (pokemon1);
+
+//Pokemon2
+    const pokemon2 = {
+    name: "Charmander",
+    height: 6,
+    types: ['fire']
+};
+pokemonList.push(pokemon2)
+
+//Pokemon 3
+const pokemon3 = {
+    name: "Squirtle",
+    height: 5,
+    types: ['water']
+};
+pokemonList.push(pokemon3)
+
+console.log (pokemonList);
