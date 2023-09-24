7ee5a5d77567bcbf2f0a038284b61dcc8d119d68
heldrethtw <141576187+heldrethtw@users.noreply.github.com>
2023-09-24 14:47:16 -0400

    Javascript 1.3

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ let pokemonList = []
 
 //Pokemon 1
 const pokemon1 =
-    {name: "Bulbasaur",
-     height: 7,
-     types: [grass, poison]
+{
+    name: "Bulbasaur",
+    height: 7,
+    types: [grass, poison]
 };
-pokemonList.push (pokemon1);
+pokemonList.push(pokemon1);
 
 //Pokemon2
-    const pokemon2 = {
+const pokemon2 = {
     name: "Charmander",
     height: 6,
     types: ['fire']
@@ -24,4 +25,22 @@ const pokemon3 = {
 };
 pokemonList.push(pokemon3)
 
-console.log (pokemonList);
+console.log(pokemonList);
+
+//set height edge for unique pokemon
+
+let specialHeight = 6;
+
+//iterate over each pokemon
+for (let i = 0; i < pokemonList.length; i++) {
+    const pokemon = pokemonList[i];
+
+    //check the height of the pokemon and write a message based on the height 
+    if (pokemon.height > specialHeight) {
+        document.write(`${pokemon.name} (height:${pokemon.height} - wow, that's big!<br>`);
+    } else if (pokemon.height > 5) {
+        document.write(`${pokemon.name} is an averaged sized pokemon.<br>`);
+    } else {
+        document.write(`${pokemon.name} is a small pokemon.<br>`);
+    }
+}
