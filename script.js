535abe4eabd82f75e86cbd64d0af71ec001a3c43
heldrethtw <141576187+heldrethtw@users.noreply.github.com>
2023-11-30 22:31:11 -0500

    Removed unecessary comments

--- script.js
+++ script.js
@@ -13,10 +13,10 @@ let pokemonRepository = (function () {
   function addListItem(pokemon) {
     let list = document.querySelector('.pokemon-list');
     let listItem = document.createElement('li');
-    listItem.classList.add('list-group-item'); // Bootstrap list item classes
+    listItem.classList.add('list-group-item'); 
     let button = document.createElement('button');
     button.innerText = pokemon.name;
-    button.classList.add('btn', 'btn-primary'); // Bootstrap button classes
+    button.classList.add('btn', 'btn-primary'); 
     button.setAttribute('data-toggle', 'modal');
     button.setAttribute('data-target', '#pokemonModal');
     button.addEventListener('click', function () {
@@ -44,38 +44,38 @@ let pokemonRepository = (function () {
 
   async function loadDetails(item) {
     let url = item.detailsUrl;
-    console.log('Loading details for:', item.name); // Add console log for debugging
+    console.log('Loading details for:', item.name); 
     return fetch(url).then(function (response) {
       return response.json();
     }).then(function (details) {
-      // Assign details to the item
+      
       item.imageUrl = details.sprites.front_default;
       item.height = details.height;
-      // You can add more details here
+      
     }).catch(function (e) {
       console.error(e);
     });
   }
 
   function showDetails(pokemon) {
-    console.log(pokemon); // Add console log for debugging
+    console.log(pokemon); 
     loadDetails(pokemon).then(function () {
-      // Fill in modal details
+      
       document.querySelector('.modal-title').innerText = pokemon.name;
       document.querySelector('.modal-body').innerHTML = `
         <p><strong>Height:</strong> ${pokemon.height}</p>
         <img src="${pokemon.imageUrl}" alt="Image of ${pokemon.name}">
       `;
-      $('#pokemonModal').modal('show'); // Use Bootstrap's modal method to show the modal
+      $('#pokemonModal').modal('show'); 
     });
   }
 
-  // Public methods and properties
+  
   return {
-    add: add, // Add a Pokemon to the list
-    getAll: getAll, // Get all the Pokemon in the list
-    addListItem: addListItem, // Add a list item for a Pokemon
-    loadList: loadList // Load the list of Pokemon from the API
+    add: add, 
+    getAll: getAll, 
+    addListItem: addListItem, 
+    loadList: loadList 
   };
 })();
 
