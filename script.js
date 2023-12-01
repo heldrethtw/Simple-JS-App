ffaf95b9685f0fecc665fedbbb06bd3a464c57c3
heldrethtw <141576187+heldrethtw@users.noreply.github.com>
2023-11-30 23:47:45 -0500

    Can't sort error for $ not defined

--- script.js
+++ script.js
@@ -1,90 +1,90 @@
-$(document).ready(function () {
 
-  let pokemonRepository = (function () {
-    let pokemonList = [];
-    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
-    function add(pokemon) {
-      pokemonList.push(pokemon);
-    }
+let pokemonRepository = (function () {
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
-    function getAll() {
-      return pokemonList;
-    }
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
 
-    function addListItem(pokemon) {
-      let list = document.querySelector('.pokemon-list');
-      let listItem = document.createElement('li');
-      listItem.classList.add('list-group-item');
-      let button = document.createElement('button');
-      button.innerText = pokemon.name;
-      button.classList.add('btn', 'btn-primary');
-      button.setAttribute('data-toggle', 'modal');
-      button.setAttribute('data-target', '#pokemonModal');
-      button.addEventListener('click', function () {
-        showDetails(pokemon);
+  function getAll() {
+    return pokemonList;
+  }
+
+  function addListItem(pokemon) {
+    let list = document.querySelector('.pokemon-list');
+    let listItem = document.createElement('li');
+    listItem.classList.add('list-group-item');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('btn', 'btn-primary');
+    button.setAttribute('data-toggle', 'modal');
+    button.setAttribute('data-target', '#pokemonModal');
+    button.addEventListener('click', function () {
+      showDetails(pokemon);
+    });
+    listItem.appendChild(button);
+    list.appendChild(listItem);
+  }
+
+  async function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
       });
-      listItem.appendChild(button);
-      list.appendChild(listItem);
-    }
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
 
-    async function loadList() {
-      return fetch(apiUrl).then(function (response) {
-        return response.json();
-      }).then(function (json) {
-        json.results.forEach(function (item) {
-          let pokemon = {
-            name: item.name,
-            detailsUrl: item.url
-          };
-          add(pokemon);
-        });
-      }).catch(function (e) {
-        console.error(e);
-      });
-    }
+  async function loadDetails(item) {
+    let url = item.detailsUrl;
 
-    async function loadDetails(item) {
-      let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
 
-      return fetch(url).then(function (response) {
-        return response.json();
-      }).then(function (details) {
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
 
-        item.imageUrl = details.sprites.front_default;
-        item.height = details.height;
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
 
-      }).catch(function (e) {
-        console.error(e);
-      });
-    }
+  function showDetails(pokemon) {
 
-    function showDetails(pokemon) {
+    loadDetails(pokemon).then(function () {
 
-      loadDetails(pokemon).then(function () {
-
-        document.querySelector('.modal-title').innerText = pokemon.name;
-        document.querySelector('.modal-body').innerHTML = `
+      document.querySelector('.modal-title').innerText = pokemon.name;
+      document.querySelector('.modal-body').innerHTML = `
         <p><strong>Height:</strong> ${pokemon.height}</p>
         <img src="${pokemon.imageUrl}" alt="Image of ${pokemon.name}">
       `;
-        $('#pokemonModal').modal('show');
-      });
-    }
-
-
-    return {
-      add: add,
-      getAll: getAll,
-      addListItem: addListItem,
-      loadList: loadList
-    };
-  })();
-
-  pokemonRepository.loadList().then(function () {
-    pokemonRepository.getAll().forEach(function (pokemon) {
-      pokemonRepository.addListItem(pokemon);
+      $('#pokemonModal').modal('show');
     });
-  });
+  }
 
+
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+    loadList: loadList
+  };
+})();
+
+pokemonRepository.loadList().then(function () {
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
 });
+
+$() 
